8a2a7cdb35c6ed4724064564deb2e12470013a3c
Glitch (hello-express) <none>
2019-11-16 16:11:44 +0000

    🕶😻 Checkpoint ./commands/refund.js:6306021/502

--- commands/refund.js
+++ commands/refund.js
@@ -15,7 +15,18 @@ function username() {
   .setTitle("New Refund Request")
   .setColor(colors.help)
   .setDescription("Your refund request will be sent, but first supply some information. \n**What is the username of the account that lost the item?**")
-  message.author.send()
+  let dm
+  try {
+    dm = message.author.createDM
+    message.author.send(embed)   
+  } catch(err) {
+    return message.channel.send("Sorry, I could not send you a DM, and no refund request could be made.")
+  }
+  let filter = m => m.author.id == message.author.id
+  let collector = dm.createMessageCollector(filter, {time: 60000})
+  let stopped = true
+  
+  
 }
 
 }
