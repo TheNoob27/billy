0221df789d334e207b3b107441652bc5ece6f819
Glitch (hello-express) <none>
2019-11-16 16:22:55 +0000

    ☂️🔋 Checkpoint ./commands/refund.js:6306021/1586

--- commands/refund.js
+++ commands/refund.js
@@ -15,6 +15,7 @@ function username() {
   .setTitle("New Refund Request")
   .setColor(colors.help)
   .setDescription("Your refund request will be sent, but first supply some information. \n**What is the username of the account that lost the item?**")
+  .setFooter("Didn't mean to send a refund request? Just say cancel to cancel.")
   let dm
   try {
     dm = message.author.createDM
@@ -25,10 +26,38 @@ function username() {
   let filter = m => m.author.id == message.author.id
   let collector = dm.createMessageCollector(filter, {time: 60000})
   let stopped = true
+  let cancelled = false
+  collector.on("collect", m => {
+    if (m.content.length > 20) return m.author.send("A valid username isn't longer than 20 characters.")
+    if (m.content.length < 3) return m.author.send("A valid username isn't shorter than 3 characters.")
+    if (m.content.includes(" ")) return m.author.send("A valid username doesn't include spaces.")
+    if (m.content.toLowerCase() == "cancel") {
+      cancelled = true
+      collector.stop()
+    }
+    stopped = false
+    refund.username = m.content
+    collector.stop()
+  })
   
-  
+  collector.on("end", () => {
+    if (cancelled) return message.author.send("Cancelled thr re")
+    if (stopped) return message.author.send("You took too long to provide a valid username.")
+    
+    date(dm)
+  })
 }
 
+  function date(dm) {
+    let embed = new Discord.RichEmbed()
+  .setTitle("New Refund Request")
+  .setColor(colors.help)
+  .setDescription("**When did you lose this item?** \n\n**Username**: "+refund.username)
+  
+    message.author.send(embed)
+  }
+  
+  
 }
 module.exports.help = {
   name: "refund",
