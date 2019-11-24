548c07ba581a60b6174bbd41d4b20170b0880680
Glitch (hello-express) <none>
2019-11-24 14:34:28 +0000

    🎇💫 Checkpoint ./commands/play.js:6306021/295

---
+++ commands/play.js
@@ -0,0 +1,13 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args, colors) => {
+
+}
+module.exports.help = {
+  name: "play",
+  aliases: ["battle"],
+  description: "Play a game that is supposed to mimic FOB",
+  usage: `b!play`,
+  category: ""
+}
