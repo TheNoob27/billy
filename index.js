7de7605a619d6fa8a9a376ffc670d5d7d37bf4c8
Glitch (hello-express) <none>
2019-11-24 17:45:28 +0000

    👜🔔 Checkpoint ./index.js:6306021/937

--- index.js
+++ index.js
@@ -294,19 +294,22 @@ client.on("guildMemberAdd", member => {
   
 })
 
-client.on("messageReactionAdd", (r, user) => {
+client.on("messageReactionAdd", async (r, user) => {
   let message = r.message
   if (message.guild.id != "648056524094046239") return
   
   if (["🍆", "🍑", "🖕"].includes(r.emoji.name)) {
     r.remove(user)
+    db.add(`badreactions_${user.id}`, 1)
+    let infs = await db.fetch(`badreactions_${user.id}`)
     let embed = new Discord.RichEmbed()
     .setAuthor(user.tag, user.displayAvatarURL)
     .setTitle("Inappropriate Reaction")
     .setColor(config.error)
-    .setDescription("**"+user.tag+"** reacted to [a message](https://discordapp.com/channels/"+message.guild.id+"/"+message.channel.id+"/"+message.id+") in <#"+message.channel.id+"> with the emoji "+r.emoji+".")
-    .setFooter("Message sent by "+ message.author.username, message.author.displayAvatarURL)
-    
+    .setDescription("**"+user.tag+"** reacted to [a message](https://discordapp.com/channels/"+message.guild.id+"/"+message.channel.id+"/"+message.id+") in <#"+message.channel.id+"> with the emoji "+r.emoji+"."+ (infs >= 3 ? "\n***This user has now been given the <@&828282828288282> role as they have reacted with inappropriate reactions too many times. They can no longer add new reactions to messages.***": ""))
+    .setFooter((3 - infs > 0 ? 3 - infs +" more times and they will be banned from adding reactions to messages." : "They are now banned from adding reactions to messages."))
+    .setTimestamp()
+    if (infs >= 3)
     user.send("That emoji is not allowed!")
     return client.channels.get("648199918632304671").send(embed)
   } else {
