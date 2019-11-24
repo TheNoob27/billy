7683b1852b9625d6f2d871b3aac56dab533c1f73
Glitch (hello-express) <none>
2019-11-24 14:46:28 +0000

    🌈🤗 Checkpoint ./commands/play.js:6306021/545 ./config.json:6306021/14

--- commands/play.js
+++ commands/play.js
@@ -3,11 +3,26 @@ const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args, colors) => {
 
+function setup() {
+  let game = {
+    players: [],
+    rounds: null
+  }
+  let embed = new Discord.RichEmbed()
+  .setColor(colors.color)
+  .setDescription("A new game is starting! React with ⚔️ to join!")
+  .addField("Players", "​")
+  message.channel.send(embed).then(msg => {
+    msg.react("⚔️")
+  })
+
+  }
+
 }
 module.exports.help = {
   name: "play",
   aliases: ["battle"],
   description: "Play a game that is supposed to mimic FOB",
   usage: `b!play`,
-  category: ""
+  category: "Soon"
 }
