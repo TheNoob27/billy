8e14c1170bac98b07e42ea28081c811522b6c2e5
Glitch (hello-express) <none>
2019-11-24 20:32:28 +0000

    🎷👩‍👩‍👦 Checkpoint ./commands/post.js:6306021/919

--- commands/post.js
+++ commands/post.js
@@ -3,15 +3,25 @@ const Discord = require('discord.js')
 module.exports.run = async (client, message, args, colors) => {
 
   let link = args[0]
-  if (!link) return message.channel.send("Please provide a Field Of Battle private server link.")
-  if (!link.startsWith("https://www.roblox.com/games/147536429/FIELD-of-BATTLE?privateServerLinkCode=")) return message.channel.send("That was not a valid private server link.")
+  if (!link) return message.channel.send("Please provide a Field of Battle private server link.")
+  if (!link.startsWith("https://www.roblox.com/games/147536429/FIELD-of-BATTLE?privateServerLinkCode=")) return message.channel.send("That was not a valid Field of Battle private server link.")
   
+  let custom = args.slice(1).join(" ")
+  
+  let embed = new Discord.RichEmbed()
+  .setColor(colors.color)
+  .setTitle("Demon Farm")
+  .setAuthor(message.author.tag, message.author.displayAvatarURL)
+  .setDescription(custom ? custom : "**"+ message.author.tag+ "** is hosting a Demon Farm! Click [here]("+link+") to join!")
+  .addField("Link", link)
+  
+ return client.channels.get("648072216444928010").send("<@&648123633658626087>", embed)
   
 }
 module.exports.help = {
   name: "post",
   aliases: ["postlink", "postfarm"],
   description: "Post a demon farm link.",
-  usage: `b!post <link>`,
+  usage: `b!post <link> (custom message)`,
   category: "FOB"
 }
