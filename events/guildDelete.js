4a91a95610a4f0cc81c5157573e78f4b1507a217
Glitch (billy-ray-joe) <none>
2019-12-21 13:28:46 +0000

    🎥⚾️ Checkpoint ./events/guildDelete.js:6306021/717

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,2 +1,20 @@
 const ms = require("parse-ms")
 const { RichEmbed } = require("discord.js")
+
+module.exports = (client, guild) => {
+let joinms = ms(Date.now() - guild.createdAt)
+let joined = `${joinms.days}d, ${joinms.hours}h and ${joinms.minutes}m old.`
+let owner = guild.owner
+if (owner) owner = guild.owner.user.tag
+else owner = "Unknown"
+  
+  let embed = new RichEmbed()
+  .setTitle("Removed!", guild.iconURL)
+  .setDescription("I have been removed from a server!")
+  .addField("Server", `Name: ${guild.name} \n Member Count: ${guild.members.size.toLocaleString()} \n Owner: ${owner} \n Server Age: ${joined}`)
+  .setColor("#ff0000")
+  .setTimestamp()
+  .setFooter(`This now brings my server count to ${client.guilds.size} :(`)
+  
+  client.channels.get("637370478339817482").send(embed)
+  }
