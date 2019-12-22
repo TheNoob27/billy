2ee414b51ad76cf24abfa7ffd1a7e40025ca65d7
Glitch (billy-ray-joe) <none>
2019-12-22 10:22:52 +0000

    🏏🚓 Checkpoint ./classes/Trade.js:6306021/485

--- classes/Trade.js
+++ classes/Trade.js
@@ -1,6 +1,26 @@
 class Trade {
-  constructor(trade1, trade2) {
+  constructor(client, trade1, trade2) {
     trade2.trade = this
     trade2.tradingWith = trade1
+    
+    this.client = client
+    this.trade1 = trade1
+    this.trade2 = trade2
+    this.items = []
+    this.checks = 0
+    
   }
+  
+  send(...msg) {
+    if (this.trade1.channel.id == this.trade2.channel.id) {
+      this.trade1.channel.send(...msg)
+    } else {
+      
+    }
+  }
+  
+  check(user) {
+    this.checks += 1
+  }
+  
 }
