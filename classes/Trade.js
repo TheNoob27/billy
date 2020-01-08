28b6607c029ede1873cb41213300c5f47a3affa7
Glitch (billy-ray-joe) <none>
2020-01-08 20:24:29 +0000

    💁🍭 Checkpoint ./classes/Trade.js:6306021/860

--- classes/Trade.js
+++ classes/Trade.js
@@ -3,17 +3,25 @@ class Trade {
     this.client = client
     this.trader = user
     this.tradingWith = user2
-    this.items1 = []
-    this.items2 = []
+    this.items = {
+      "1": [],
+      "2": []
+    }
   }
   
   addItem(item = {}, slot = 1) {
-    if (item.name) return false;
-    
+    if (!item.name) return false;
+    if (typeof slot == "number") slot = String(slot)
+
     if (item.name !== "Gold") {
-      
+      let exists = this.items[slot].some(i => i.name == item.name)
+      if (exists) this.items[slot].find(i => i.name == item.name).amount += item.amount
+      else this.items[slot].push(item)
     } else {
-      
+      let exists = this.items[slot].some(i => i.name == "Gold")
+      if (exists) this.items[slot].find(i => i.name == "Gold").amount += item.amount
+      else this.items[slot].push(item)
     }
+    return true
   }
 }
