b71a868024eb94f8befeb8c923a735f5544ae93b
Glitch (billy-ray-joe) <none>
2020-01-08 20:13:21 +0000

    💠🏜 Checkpoint ./classes/Trade.js:6306021/315

--- classes/Trade.js
+++ classes/Trade.js
@@ -1,5 +1,19 @@
 class Trade {
-  constructor(user, user2) {
+  constructor(client, user, user2) {
+    this.client = client
+    this.trader = user
+    this.tradingWith = user2
+    this.items1 = []
+    this.items2 = []
+  }
+  
+  addItem(item = {}, slot = 1) {
+    if (item.name) return false;
     
+    if (item.name !== "Gold") {
+      
+    } else {
+      
+    }
   }
 }
