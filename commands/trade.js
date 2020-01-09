d588eb29851091731cf21a583cef79e15e08207f
Glitch (billy-ray-joe) <none>
2020-01-09 18:14:58 +0000

    🖖🚴 Checkpoint ./commands/trade.js:6306021/1169

--- commands/trade.js
+++ commands/trade.js
@@ -1,8 +1,9 @@
 const { RichEmbed } = require("discord.js")
 const Command = require("../classes/Command.js")
 const { finduser } = require("../functions.js")
+const Trade = require("../classes/Trade.js")
 
-class Trade extends Command {
+class TradeCMD extends Command {
   constructor(client) {
     super(client, {
       name: "trade",
@@ -21,9 +22,32 @@ class Trade extends Command {
     
     if (!user) return message.channel.send("Couldn't find that user.")
     
+    message.channel.send(
+      new RichEmbed()
+      .setTitle("New Trade")
+      .setColor(colors.color)
+      .setDescription("Do you accept **" + message.author.tag + "**'s trade request?")
+    )
+    let filter = (r, u) => u.id == user.id && ["✅", "❌"].includes(r.emoji.name)
+    let collector = message.channel.createMessageCollector(filter, {time: 600000})
+    let accepted = false
+    collector.once("collect", r => {
+      if (r.emoji == "✅") {
+        accepted = true
+        collector.stop()
+      } else {
+        collector.stop()
+      }
+    })
     
-    
+    collector.once("end", () => {
+      if (accepted) {
+        let trade = new Trade(message.author, user)
+        
+      }
+      return message.reply("They did not accept your request.")
+    })
   }
 }
 
-module.exports = Trade
+module.exports = TradeCMD
