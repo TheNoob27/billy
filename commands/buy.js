1ecad9881152809a1ea11f29b9922fd56fc57652
Glitch (billy-ray-joe) <none>
2020-02-16 21:28:40 +0000

    🍴⛷ Checkpoint ./commands/buy.js:6306021/1046

--- commands/buy.js
+++ commands/buy.js
@@ -21,11 +21,24 @@ class Buy extends Command {
     if (!inventory.gold) inventory.gold = 0
     
     
-    let canBuy = item => inventory.gold > item.cost && item.gemsneeded.every(g => inventory.gems[g] > 0) && (inventory.sword.name !== item.name && inventory.armour.name !== item.name)
+    let canBuy = item => inventory.gold >= item.cost && item.gemsneeded.every(g => inventory.gems[g] > 0) && (inventory.sword.name !== item.name && inventory.armour.name !== item.name)
     let reason = item => 
-    inventory.gold > item.cost ? "You do not have enough gold to purchase this item. You currently have " + inventory.gold.toLocaleString() + "." :
-    item.gemsneeded.every(g => inventory.gems[g] > 0) ? "You do not have the required gems to purchase this item. Gems needed: "+item.gemsneeded.map(g => ) (inventory.sword.name !== item.name && inventory.armour.name !== item.name)
+    inventory.gold < item.cost ? "You do not have enough gold to purchase this item. You need " + (item.cost - inventory.gold).toLocaleString() + " more gold." :
+    item.gemsneeded.every(g => inventory.gems[g] <= 0) ? "You do not have the required gems to purchase this item. Gems required: "+item.gemsneeded.map(g => "**" + client.resolveGem(g).name + "**, ").join("") :
+    "You already own this item."
     
+    let item = client.resolveItem(args[0])
+    if (!item) return message.channel.send("Sorry, I couldn't find the item you were looking for.")
+    
+    if (canBuy(item)) {
+      client.fob.subtract(message.author.id + ".inventory.gold", item.cost)
+      let embed = new RichEmbed()
+      .setTitle("Purchase Successful")
+      .setDescription("Successfully bought **" + item.name + "** for " + item.cost.toLocaleString() + " gold!")
+      .setColor(colors.color)
+    } else {
+      let why = reason(item)
+    }
   }
 }
 
