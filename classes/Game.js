344a3410bfc20bdf0a8a61b9f806849eee56e351
Glitch (billy-ray-joe) <none>
2020-02-17 10:31:00 +0000

    📫🚿 Checkpoint ./classes/Game.js:6306021/2043

--- classes/Game.js
+++ classes/Game.js
@@ -10,6 +10,7 @@ class Game {
     this.enemy = null
     this.enemyteam = []
     this.regen = null
+    this.collector = null
     this.ended = false
   }
   
@@ -28,6 +29,7 @@ class Game {
       hp: (18 * (level - 1) + 100) + (inv.armour.health || 0),
       maxhp: (18 * (level - 1) + 100) + (inv.armour.health || 0)
     }
+    if (armour.name) push.armour = armour
     
     this.players.set(push.id, push)
     return this
@@ -43,10 +45,79 @@ class Game {
     setTimeout(() => {
       this.addPlayer(user)
     }, time)
+    return this
+  }
+
+  attackEnemy(player) {
+    if (!this.enemy || !this.collector) return null
     
+    this.enemy.hp -= player.damage
+    if (this.enemy.hp <= 0) return this.collector.stop("dead")
+    
+    return this
+  }
+  
+  attackPlayer(player) {
+    if (!this.enemy || !this.collector) return null
+    
+    player.hp -= this.enemy.damage
   }
   
   spawnEnemy(enemy) {
+    if (enemy) {
+      this.enemy = enemy
+      return this.enemy
+    }
     
+    enemy = {
+      name: null,
+      hp: null,
+      damage: null
+    }
+    
+    let enemies = this.enemyteam
+    enemy.name = enemies[Math.floor(Math.random() * enemies.length)]
+    
+  
+    let e = enemy.name
+    
+    if (e == "Soldier" || e == "Grunt") {
+      enemy.hp = 100
+      enemy.damage = 11
+    } else if (e == "Mage") {
+      enemy.hp = 135
+      enemy.damage = 16
+    } else if (e == "Archer") {
+      enemy.hp = 145
+      enemy.damage = 15
+    } else if (e == "Knight" || e == "Smasher") {
+      enemy.hp = 169
+      enemy.damage = 18
+    } else if (e == "Captain" || e == "Warrior") {
+      enemy.hp = 195
+      enemy.damage = 22
+    } else if (e == "Blademaster") {
+      enemy.hp = 210
+      enemy.damage = 25
+    } else if (e == "Guard" || e == "Elite Blademaster") {
+      enemy.hp = 235
+      enemy.damage = 27
+    } else if (e == "Assassin") {
+      enemy.hp = 240
+      enemy.damage = 28
+    } else if (e == "Warlord") {
+      enemy.hp = 250
+      enemy.damage = 28
+    } else if (e == "Tyrant" || e == "Giant") {
+      enemy.hp = 405
+      enemy.damage = 33
+    } else if (e == "KorKron Elite" || e == "Royal Guard") {
+      enemy.hp = 300
+      enemy.damage = 40
+    }
+    
+    this.enemy = enemy
+    
+    return this.enemy
   }
 }
