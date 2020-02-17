e897d52e686bd5c070f20e99d3b5b99863939de4
Glitch (billy-ray-joe) <none>
2020-02-17 10:18:56 +0000

    🏍👸 Checkpoint ./classes/Game.js:6306021/2468

--- classes/Game.js
+++ classes/Game.js
@@ -5,25 +5,48 @@ class Game {
     this.client = client
     
     this.players = new Collection()
-    
     this.rounds = null
     this.team = null
+    this.enemy = null
     this.enemyteam = []
     this.regen = null
     this.ended = false
   }
   
   addPlayer(user) {
-    let level = this.client.fob.fetch(`${user.id}.level.level`) || 1
-    let dmg = this.client.fob.fetch(`${user.id}.inventory.sword.damage`) || 11
-    this.players.push({
+    let data = this.client.fob.fetch(user.id);
+    let inv = data.inv || {}
+    let level = data.level || {}
+    let sword = inv.sword || {}
+    let armour = inv.armour || {}
+    
+    let push = {
       id: user.id,
-      level: level,
-      hp: (18 * (level - 1) + 100),
       tag: user.tag,
-      damage: dmg,
-      maxhp: (18 * (level - 1) + 100)
-    })
-    this.playerlist.push(user.id)
+      level: level.level,
+      damage: inv.sword.damage || 8,
+      hp: (18 * (level - 1) + 100) + (inv.armour.health || 0),
+      maxhp: (18 * (level - 1) + 100) + (inv.armour.health || 0)
+    }
+    
+    this.players.set(push.id, push)
+    return this
+  }
+  
+  removePlayer(user) {
+    this.players.delete(user.id)
+    return this
+  }
+  
+  respawnPlayer(user, time = 7000) {
+    this.players.delete(user.id)
+    setTimeout(() => {
+      this.addPlayer(user)
+    }, time)
+    
+  }
+  
+  spawnEnemy(enemy) {
+    
   }
 }
